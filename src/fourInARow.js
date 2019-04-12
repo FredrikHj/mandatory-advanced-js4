e997738f73ef16a671ad80bfade195640e26fbf1
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-04-12 15:58:10 +0200

    Klar 20%

--- src/fourInARow.js
+++ src/fourInARow.js
@@ -1,69 +1,81 @@
 import React, { PureComponent } from 'react';
-import { mainWindowCSS, gameCellCSS } from './fourInARowCSS';
-let countCol = 0;
-let arrColummContent = [];
-let gameGrids = [];
-let countRow = 0;
-
- function GameColummStructure(props) {
+import { mainWindowCSS, inGameCSS } from './fourInARowCSS';
+//let rowCount = 0;
+function GameColummStructure(props) {
   console.log(props);
+  let colRowId = props.colKey;
   return(
-    <div key={ props.colKey }>
+    <div key={ colRowId }>
       { props.colContent }
     </div>
   );
-
+  
 } 
 function GameCell(props) {
-  console.log(props);
   return(
-    <div className={ gameCellCSS.cell } key={ props.cell }></div>
-  );
-
-}
-/* class GameBoard extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-
+    <div className={ inGameCSS.cell } key={ props.cell }></div>
     );
   }
-} */
-
-class FourInARow extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.createRow = this.createRow.bind(this);
-  }
-  createRow() {
-    countCol += 1;
-    for (let row = 1; row <= 6; row++) {
-      countRow += 1;      
-      arrColummContent.push(
-        <GameCell
-          key={ countCol + 'x' +countRow }
-
-        />
-      );
+  class FourInARow extends PureComponent {
+    constructor(props) {
+      super(props);
+      
+      this.createGameGrid = this.createGameGrid.bind(this);
     }
-    return <GameColummStructure
-      colKey={ 'col'+ countCol }
-      colContent={ arrColummContent }
-    />
-
-    return arrColummContent;
+    componentDidMount() {
+      
+    }
+    createGameGrid() {
+      let arrColContent = [];
+      let arrGameGrid = [];
+      let countCol  = 0;
+      let rowInCol = 0;
+      let totRow = 6;
+      let totCol = 7;
+      rowInCol = 1;
+      
+      /* Creating a col and give it name col1/2/3...............
+      Inside the cols it are creating a couple of rows with the nr of row I need for the game*/
+      
+      for (let row = 1; row <= totRow; row++) {  
+        arrColContent.push( <GameCell 
+          key={ 
+            //rowInCol + 'x' + 
+            row }
+        /> );  
+      }
+      
+      // Save the whole col in a array and multiply it which I need for the game
+    //countCol = 0;
+    for (let col = 1; col <= totCol; col++) {
+      countCol += 1;
+      arrGameGrid.push(<GameColummStructure colKey={ 'col'+ countCol } colContent={ arrColContent }/> );
+      rowInCol += 1;
+    }
+    return arrGameGrid;
+    // ======================================================
   }
   render() {
-    gameGrids.push(this.createRow());
-    console.log(arrColummContent);
-    
     return (
-      <div className={ mainWindowCSS.bodyFrame }>
-        { gameGrids }
-      </div>
+      <section className={ mainWindowCSS.bodyFrame }>
+        <p className={  mainWindowCSS.pagesHeadLine }> Connect four</p>
+        <hr className={ mainWindowCSS.topLine }/>
+        <p className={  mainWindowCSS.wonSubLine }> Player ? Won</p>
+        <section className={ mainWindowCSS.gameGridFrame }>
+          <section className={  mainWindowCSS.playerContainer }>
+            <p>Player 1</p>
+            <p>Player 2</p>
+          </section>
+          <div className={ mainWindowCSS.gameGrid }>
+            { 
+              this.createGameGrid()
+            }
+          </div>
+          <button className={ mainWindowCSS.rstBtn }>Reset Game</button>
+        </section>
+          <div className={ inGameCSS.player1Disc }></div>
+          <div className={ inGameCSS.player2Disc }></div>
+      </section>
     );
   }
 }
--- src/fourInARowCSS.js
+++ src/fourInARowCSS.js
@@ -8,35 +8,89 @@ import { css } from 'glamor';
 } */
 /* ========================= Generall ========================= */
 export const mainWindowCSS = {
-  bodyFrame: css({
+  pagesHeadLine: css({
+   'textAlign': 'center',
+   'width': '100%',
+   'letterSpacing': '5px',
+   'fontWeight': 'bold',
+   'fontSize': '20px',
+  }),
+  topLine: css({
+    'width': '75%',
+  }),
+  wonSubLine: css({
+    'textAlign': 'center',
+    'width': '100%',
+    'letterSpacing': '3px',
+  }),
+  playerContainer: css({
     'position': 'relative',
-    'marginLeft': 'calc(50% - 175px)',
+    'left': '10px',
+    'top': '150px',
+    'width': '100px',
+
+  }),
+  bodyFrame: css({
+    'marginLeft': 'calc(50% - 275px)',
     'marginTop': '10%',
+    'backgroundColor': 'orange',
+    'borderRadius': '10px',
+    'border': '1px solid lightgrey',
     'width': '450px',
-    'height': '450px',
+    'height': '396px',
+  }),
+  gameGridFrame: css({
+    'position': 'relative',
+    'top': '-64px',
+
+  }),
+  gameGrid: css({
+    'position': 'relative',
+    'left': 'calc(57% - 175px)',
+    'display': 'flex',
+    'flexDirection': 'row',
+    'justifyContent': 'spaceAround',
     'backgroundColor': 'darkblue',
     'borderRadius': '10px',
     'border': '1px solid lightgrey',
     'width': '350px',
   }),
+  rstBtn: css({
+    'position': 'relative',
+    'left': 'calc(50% - 50px)',
+    'top': '16px',
+    'width': '100px',
+  })
 }
   
 /* ========================= Header ========================= */
 /* export const headerCSS = {  
-  pagesHeadLine: css({
-   'position': 'relative',
-   'textAlign': 'center',
-   'width': '100%',
-  }),
+
 } */
 /* ========================= Gamecell ========================= */
-export const gameCellCSS = {
+export const inGameCSS = {
   cell: css({
-    'margin': '30px',
-    'width': '30px',
-    'height': '30px',
+    'margin': '15px',
+    'width': '20px',
+    'height': '20px',
     'borderRadius': '20px', 
     'backgroundColor': 'white',
-    
+  }),
+  player1Disc: css({
+    'width': '17.5px',
+    'height': '17.5px',
+    'borderRadius': '20px', 
+    'backgroundColor': 'red',
+    'zIndex': '1',
+  }),
+  player2Disc: css({
+    'width': '17.5px',
+    'height': '17.5px',
+    'borderRadius': '20px', 
+    'backgroundColor': 'green',
+    'zIndex': '1',
+    'position': 'relative',
+    'left': '99px',
+    'top': '-348',
   }),
 }
